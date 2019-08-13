cded822285f3d2e1e455edd15ab08bb86eb4cb41
az <xuchenzhe0211@gmail.com>
2019-08-12 22:46:45 -0700

    Updates

---
+++ precache-manifest.81d4a816334b5aeccdbb4af93a99afa4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4978ce59e5c0d134b03a2487740dc52d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "7882aeaa6e853ef42df6",
+    "url": "/home/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "6d1196a384b8f3649c89",
+    "url": "/home/static/css/main.003aee3a.chunk.css"
+  },
+  {
+    "revision": "7882aeaa6e853ef42df6",
+    "url": "/home/static/js/2.0aad3bc9.chunk.js"
+  },
+  {
+    "revision": "6d1196a384b8f3649c89",
+    "url": "/home/static/js/main.79efec63.chunk.js"
+  },
+  {
+    "revision": "6f8d306b8457b8591a11",
+    "url": "/home/static/js/runtime~main.651e27bc.js"
+  },
+  {
+    "revision": "7d70522b1669e6116d93bb8333e0b1f3",
+    "url": "/home/static/media/2019-04-16-how-to-install-github-on-ubuntu.7d70522b.md"
+  },
+  {
+    "revision": "8a89147cda17d21e42814580b46a3b80",
+    "url": "/home/static/media/2019-04-16-how-to-install-mysql-on-ubuntu.8a89147c.md"
+  },
+  {
+    "revision": "ffa9349855ff862f767d5984e5c44e9f",
+    "url": "/home/static/media/2019-04-17-how-to-use-git.ffa93498.md"
+  },
+  {
+    "revision": "f16a69c2bd0e720243bf934959e7de95",
+    "url": "/home/static/media/2019-08-06-sql-study-notes.f16a69c2.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d3e6d06068dea6d964a4146304525303.js"
+  "/home/precache-manifest.81d4a816334b5aeccdbb4af93a99afa4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
